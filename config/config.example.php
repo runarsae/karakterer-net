28ad3c902c1a323fa10b3fa8517c669bee8c31df
Runar Sæther <runarsather@hotmail.com>
2022-01-25 01:56:05 +0100

    Delete old project files (#1)

--- config/config.example.php
+++
@@ -1,5 +0,0 @@
-<?php
-	function getLink() {
-		return new mysqli("HOST", "USERNAME", "PASSWORD", "DB");
-	}
-?>
--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-        <div class="container">
-			&copy; Copyright <?php echo date("Y"); ?>, KARAKTERER.net
-			<br>NTNU er ikke ansvarlig for innholdet på denne nettsiden
-        </div>
-</footer>
