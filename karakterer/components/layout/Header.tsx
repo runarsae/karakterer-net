cebea27e9a93c27a3dd60e31f90fa97f3c9937c7
Runar Sæther <runarsather@hotmail.com>
2022-09-06 21:55:22 +0200

    Add header with text logo (#38)

--- karakterer/components/layout/Header.tsx
+++ karakterer/components/layout/Header.tsx
@@ -1,56 +1,40 @@
-import Typography from 'components/common/Typography';
-import styled, { useTheme } from 'styled-components';
+import Section from 'components/common/Section';
+import Link from 'next/link';
+import styled from 'styled-components';
+import Navigation from './Navigation';
 
-const Wrapper = styled.div({
-    width: '100%'
-});
-
-const Content = styled.div((props) => ({
-    padding: '16px',
+const Content = styled(Section)({
     maxWidth: '1440px',
     width: '100%',
     margin: '0 auto',
     display: 'grid',
     gridTemplateColumns: '1fr auto',
     alignItems: 'center',
-    gap: '16px',
+    paddingBottom: 0
+});
 
-    [`@media (min-width: ${props.theme.breakpoints.md}px)`]: {
-        padding: '64px 32px 0 32px',
-        gap: '16px'
+const LogoText = styled.span((props) => ({
+    cursor: 'pointer',
+    userSelect: 'none',
+    fontFamily: 'Poppins',
+    fontSize: '20px',
+    color: props.theme.palette.logo,
+    transition: `color ${props.theme.transitionDuration}ms ease-in-out`,
+
+    '@media (hover: hover)': {
+        ':hover': {
+            color: props.theme.palette.heading
+        }
     }
 }));
 
-const Logo = styled.div({
-    display: 'flex',
-    gap: '3px',
-    alignItems: 'end'
-});
-
-const Dot = styled.div((props) => ({
-    width: '6px',
-    height: '6px',
-    borderRadius: '50%',
-    background: props.theme.palette.primary.main,
-    marginBottom: '5px'
-}));
-
 function Header() {
-    const theme = useTheme();
     return (
-        <Wrapper>
-            <Content>
-                <Logo>
-                    <Typography variant="h1" style={{ fontSize: '22px' }}>
-                        karakterer
-                    </Typography>
-                    <Dot />
-                    <Typography variant="h1" style={{ fontSize: '22px' }}>
-                        net
-                    </Typography>
-                </Logo>
-            </Content>
-        </Wrapper>
+        <Content>
+            <Link href="/">
+                <LogoText>karakterer.net</LogoText>
+            </Link>
+        </Content>
     );
 }
 
