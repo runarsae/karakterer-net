291e53c363ca5a4abcf4e7670570324a818a8f74
Runar Sæther <runarsather@hotmail.com>
2022-11-03 22:02:23 +0100

    Change view date to use date type (#120)

--- lib/upsertCourseView.ts
+++ lib/upsertCourseView.ts
@@ -1,24 +1,9 @@
 import prisma from 'lib/prisma';
 
 export async function upsertCourseView(course: string) {
-    const now = new Intl.DateTimeFormat('en-GB').format(new Date());
-
-    return prisma.view.upsert({
-        where: {
-            course_date: {
-                course: course,
-                date: now
-            }
-        },
-        update: {
-            count: {
-                increment: 1
-            }
-        },
-        create: {
-            course: course,
-            date: now,
-            count: 1
-        }
-    });
+    return prisma.$executeRaw`
+        INSERT INTO views (course, date, count) 
+        VALUES (${course}, CURRENT_DATE, 1) 
+        ON DUPLICATE KEY UPDATE count = count + 1;
+    `;
 }
