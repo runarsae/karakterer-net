cacf9e491abfd4745f0a43dfb8f0b8b260cbf550
fredrir <fhansteen@gmail.com>
2024-12-07 14:28:48 +0000

    fix CourseCard styling

--- src/components/home/mostPopularCourses/CourseCard.tsx
+++ src/components/home/mostPopularCourses/CourseCard.tsx
@@ -18,23 +18,32 @@ interface CourseCardProps {
 export default function CourseCard({ code, name, moreInfo }: CourseCardProps) {
   return (
     <Link href={`/courses/${code}`}>
-      <div className="card rounded-lg bg-gray-800 p-4 shadow-md transition-colors hover:bg-neutral-900">
-        <h2 className="text-lg font-semibold text-blue-400">{code}</h2>
-        <p className="mb-2 text-neutral-300">{name}</p>
-        <div className="grid grid-cols-2 gap-2 text-sm">
-          <div className="text-gray-200">Snittkarakter:</div>
-          <div className="font-medium text-gray-300">
-            {moreInfo?.avgGrade.toFixed(2)}
+      <div className="card transition-colors hover:bg-neutral-900">
+        <p className="text-sm">{code}</p>
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-neutral-300">
+          {name}
+        </p>
+
+        {moreInfo && (
+          <div className="grid grid-cols-2 gap-2 text-sm">
+            <div className="text-gray-200">Snittkarakter:</div>
+            <div className="font-medium text-gray-300">
+              {moreInfo?.avgGrade.toFixed(2)}
+            </div>
+            <div className="text-gray-100">Stryk %:</div>
+            <div className="font-medium text-gray-300">
+              {moreInfo?.failPercentage.toFixed(2)}%
+            </div>
+            <div className="text-gray-100">Studenter:</div>
+            <div className="font-medium text-gray-300">
+              {moreInfo?.students}
+            </div>
+            <div className="text-gray-100">Semester:</div>
+            <div className="font-medium text-gray-300">
+              {moreInfo?.semester}
+            </div>
           </div>
-          <div className="text-gray-100">Stryk %:</div>
-          <div className="font-medium text-gray-300">
-            {moreInfo?.failPercentage.toFixed(2)}%
-          </div>
-          <div className="text-gray-100">Studenter:</div>
-          <div className="font-medium text-gray-300">{moreInfo?.students}</div>
-          <div className="text-gray-100">Semester:</div>
-          <div className="font-medium text-gray-300">{moreInfo?.semester}</div>
-        </div>
+        )}
       </div>
     </Link>
   );
